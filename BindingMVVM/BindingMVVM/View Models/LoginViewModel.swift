8340529cd264c85c736c81f5c5e0e0633e41bdd0
burakaltintas011801 <burak.altintas@ozu.edu.tr>
2023-04-08 01:43:59 +0300

    View to View Model Binding implemented

---
+++ BindingMVVM/BindingMVVM/View Models/LoginViewModel.swift
@@ -0,0 +1,16 @@
+//
+//  LoginViewModel.swift
+//  BindingMVVM
+//
+//  Created by Compro on 8.04.2023.
+//
+
+import Foundation
+
+struct LoginViewModel
+{
+    var username : String = ""
+    var password : String = ""
+    
+    
+}
