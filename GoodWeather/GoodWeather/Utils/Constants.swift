81082a0d5ff104f51b88af3a760a7ba47b5bb342
burakaltintas011801 <burak.altintas@ozu.edu.tr>
2023-04-07 18:16:32 +0300

    The Good Weather App has been implemented

---
+++ GoodWeather/GoodWeather/Utils/Constants.swift
@@ -0,0 +1,28 @@
+//
+//  Constants.swift
+//  GoodWeather
+//
+//  Created by Mohammad Azam on 3/3/21.
+//  Copyright © 2021 Mohammad Azam. All rights reserved.
+//
+
+import Foundation
+
+struct Constants {
+    
+    struct Urls {
+        
+        static func urlForWeatherByCity(city: String) -> URL {
+            
+            // get the default settings for temperature
+            let userDefaults = UserDefaults.standard
+            let unit = (userDefaults.value(forKey: "unit") as? String) ?? "imperial"
+            
+            return URL(string: "https://api.openweathermap.org/data/2.5/weather?q=\(city.escaped())&appid=ed2d94aac1dad62e2b6473aed16db171&units=\(unit)")!
+            
+        
+        }
+        
+    }
+    
+}
